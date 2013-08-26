2af60d820197d1f43b5f985c1cc364234cfe7573
Matthew Smith <matt@m0smith.com>
2013-08-26 10:04:11 -0600

    Demo from class

--- src/game1400/Game1400_1.java
+++ src/game1400/Game1400_1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 /**
  *  This is a game of tic-tac-toe.
  * 
+ * https://www.github.com/m0smith/cs1400
+ * 
  * This version demonstrates:<ul>
  * <li>output (println, print, printf)
  * <li>input (Scanner)
@@ -20,9 +22,9 @@ public class Game1400_1 {
      */
     
     public static void main(String[] args) {
-        int score;
+        int score = 0;
         String header = "How about a nice game of tic-tac-toe?";
-        
+               
         // Show the board
         System.out.println(header);
         System.out.println("1|2|3");
@@ -32,12 +34,13 @@ public class Game1400_1 {
         System.out.println("7|8|9");
         
         // Ask user for move
-        System.out.print("Enter you move =>");
+        System.out.print("It is your turn;\nEnter your move =>");
         Scanner scanner = new Scanner(System.in);
         int position = scanner.nextInt();
         
         // Compute score (nonsense)
-        score = ((position + 5) * 2 - 10) % 6;
-        System.out.printf("You entered %d and your score is %d\n", position, score);
+        int subscore = position + score + 10;
+        score = (subscore * 2 - 10) % 6;
+        System.out.printf("You entered %-10d and your score is %d %-10s.\n", position, score, "Dude!!");
     }
 }
