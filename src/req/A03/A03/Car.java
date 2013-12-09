6f22b0ff716a3b596151e451e197dfdb41085ea4
Matthew O. Smith <matt@m0smith.com>
2013-12-09 09:29:10 -0700

    Added required stuff for A03

---
+++ src/req/A03/A03/Car.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A03;
+
+/*******************************************************
+* Sample Code - do not modify
+* Assignment: A03
+*******************************************************/
+
+public class Car
+{
+   // fields
+	private String make;
+	private String model;
+	private String owner;
+	
+	// contructors
+	
+	// methods
+	public String getMake()
+	{
+		return make;
+	}	
+	
+	public String getModel()
+	{
+		return model;
+	}
+	
+	public String getOwner()
+	{
+		return owner;
+	}
+	
+	public void setMake(String newMake)
+	{
+		make = newMake;
+	}
+	
+	public void setModel(String newModel)
+	{
+		model = newModel;
+	}
+	
+	public void setOwner(String newOwner)
+	{
+		owner = newOwner;
+	}
+}
---
+++ src/req/A03/A03/Country.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A03;
+
+/*******************************************************
+* Sample Code - do not modify
+* Assignment: A03
+*******************************************************/
+
+public class Country
+{
+   // fields
+	private String name;
+	private int population; 
+	private int area; // in square miles
+	
+	// constructors
+	
+	// methods
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getPopulation()
+	{
+		return population;
+	}	
+	
+	public int getArea()
+	{
+		return area;
+	}	
+	
+	public void setName(String newName)
+	{
+		name = newName;
+	}
+
+	public void setPopulation(int newPopulation)
+	{
+		population = newPopulation;
+	}
+
+	public void setArea(int newArea)
+	{
+		area = newArea;
+	}
+	
+	// population per square mile
+	public int populationDensity()
+	{
+		int density;
+		density = population / area; // integer division truncates
+		return density;
+	}
+}
